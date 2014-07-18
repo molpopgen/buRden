9d571e7cd9cf2a6d12254081e9e6e5adf3fb64c8
Kevin R Thornton <krthornt@thornton01.bio.uci.edu>
2014-07-18 16:10:16 -0700

    chisquared details

--- src/chisq_per_marker.cc
+++ src/chisq_per_marker.cc
@@ -29,6 +29,7 @@ double get_log10_chisq(const unsigned ctable[4])
 //' @param ccdata A matrix of markers (columns) and individuals (rows).  Data are coded as the number of copies of the minor allele.
 //' @param ccstatus A vector of binary phenotype labels.  0 = control, 1 = case.
 //' @return A vector of -log10(p-values) from a chi-squared test with one degree of freedom.  The chisq test is based on a 2x2 table of minor vs major allele counts in cases vs. controls.
+//' @details The chi-squared values have Yate's continuity correction applied
 //' @examples
 //' data(rec.ccdata)
 //' status = c(rep(0,rec.ccdata$ncontrols),rep(1,rec.ccdata$ncases))
