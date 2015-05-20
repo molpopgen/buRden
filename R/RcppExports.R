d795cdfbbd27ecbceb5df896c55e5ef690b8bd9d
Kevin R. Thornton <krthornt@uci.edu>
2015-05-20 12:46:42 -0700

    chisq_per_marker now usable as regular C++ fxn and as R fxn

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -99,17 +99,7 @@ chisq <- function(a, b, c, d, yates) {
     .Call('buRden_chisq', PACKAGE = 'buRden', a, b, c, d, yates)
 }
 
-#' Single-marker association test based on the chi-squared statistic
-#' @param ccdata A matrix of markers (columns) and individuals (rows).  Data are coded as the number of copies of the minor allele.
-#' @param ccstatus A vector of binary phenotype labels.  0 = control, 1 = case.
-#' @param yates Apply continuity correction?
-#' @return A vector of -log10(p-values) from a chi-squared test with one degree of freedom.  The chisq test is based on a 2x2 table of minor vs major allele counts in cases vs. controls.
-#' @examples
-#' data(rec.ccdata)
-#' status = c(rep(0,rec.ccdata$ncontrols),rep(1,rec.ccdata$ncases))
-#' #Note that the result should be very very similar to logistic regression under additive model...
-#' rec.ccdata.chisq = chisq_per_marker(rec.ccdata$genos, status)
-chisq_per_marker <- function(ccdata, ccstatus, yates) {
+.chisq_per_marker <- function(ccdata, ccstatus, yates) {
     .Call('buRden_chisq_per_marker', PACKAGE = 'buRden', ccdata, ccstatus, yates)
 }
 
--- src/chisq_per_marker.cc
+++ src/chisq_per_marker.cc
@@ -8,17 +8,7 @@
 using namespace std;
 using namespace Rcpp;
 
-//' Single-marker association test based on the chi-squared statistic
-//' @param ccdata A matrix of markers (columns) and individuals (rows).  Data are coded as the number of copies of the minor allele.
-//' @param ccstatus A vector of binary phenotype labels.  0 = control, 1 = case.
-//' @param yates Apply continuity correction?
-//' @return A vector of -log10(p-values) from a chi-squared test with one degree of freedom.  The chisq test is based on a 2x2 table of minor vs major allele counts in cases vs. controls.
-//' @examples
-//' data(rec.ccdata)
-//' status = c(rep(0,rec.ccdata$ncontrols),rep(1,rec.ccdata$ncases))
-//' #Note that the result should be very very similar to logistic regression under additive model...
-//' rec.ccdata.chisq = chisq_per_marker(rec.ccdata$genos, status)
-// [[Rcpp::export]]
+// [[Rcpp::export(".chisq_per_marker")]]
 NumericVector chisq_per_marker( const IntegerMatrix & ccdata,
 				const IntegerVector & ccstatus,
 				const bool & yates)
